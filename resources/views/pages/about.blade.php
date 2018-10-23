d1c0fec605ee423c396a024498ba087a779c84a2
alexandraflores <af345@njit.edu>
2018-10-22 23:22:40 -0400

    Task: created contact page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,8 +2,11 @@
 
 @section('content')
 
-    <h1>This is my About Page</h1>
-    <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+
+
+
+        <h1>This is my About Page</h1>
+        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
 
 
 @endsection
