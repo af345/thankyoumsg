889f69838d0badbaf69276f15a931b6ad9fda799
alexandraflores <af345@njit.edu>
2018-10-23 23:28:06 -0400

    Task: print name and message

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', 'PagesController@home') ->name('home');
 Route::get('/about', 'PagesController@about')->name('about');
 Route::get('/contact', 'PagesController@contact')->name('contact');
 Route::post('/contact', 'PagesController@store')->name('contact.store');
+Route::get('/thanks/{name}', 'PagesController@thanks')->name('thanks');
+
 /*Route::post('/contact', function () {
     $data = request() ->all();
     echo "Name: " . $data['name']. '<br>';
