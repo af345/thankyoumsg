48108aaf4e603559354f136b7163b3fc5b477272
alexandraflores <af345@njit.edu>
2018-10-23 22:23:45 -0400

    Task: added pagescontroller

--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('pages.index');
-})->name('home');
-Route::get('/about', function () {
-    return view('pages/about');
-})->name('about');
-Route::get('/contact', function () {
-    return view('pages.contact');
-})->name('contact');
+Route::get('/', 'PagesController@home') ->name('home');
+Route::get('/about', 'PagesController@about')->name('about');
+Route::get('/contact', 'PagesController@contact')->name('contact');
 Route::post('/contact', function () {
     $data = request() ->all();
     echo "Email: " . $data['email']. '<br>';
